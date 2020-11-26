f88bef2c4810b7e19e2e1cb96be89242f87967b4
Trent Johnson <56526329+tjohnstone3@users.noreply.github.com>
2020-11-25 18:47:26 -0600

    invert negative if statements isolate methods extract common methods create constants

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,9 @@
 package com.gildedrose;
 
 class GildedRose {
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -9,54 +12,75 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
+            updateOneItem(item);
         }
     }
+
+    private void updateOneItem(Item item) {
+        updateItemQuality(item);
+
+        updateItemSellIn(item);
+
+        if (hasExpired(item)) {
+            updateExpired(item);
+        }
+    }
+
+    private void updateItemQuality(Item item) {
+        if (item.name.equals(AGED_BRIE)) {
+            incrementQuality(item);
+        } else if (item.name.equals(BACKSTAGE_PASSES)) {
+            incrementQuality(item);
+            if (item.sellIn < 11) {
+                incrementQuality(item);
+            }
+            if (item.sellIn < 6) {
+                incrementQuality(item);
+            }
+        } else if (item.name.equals(SULFURAS)) {
+        } else decrementQuality(item);
+    }
+
+    private void updateItemSellIn(Item item) {
+        if (item.name.equals(SULFURAS)) {
+        } else {
+            item.sellIn -= 1;
+        }
+    }
+
+    private void updateExpired(Item item) {
+        if (item.name.equals(AGED_BRIE)) {
+            incrementQuality(item);
+        } else if (item.name.equals(BACKSTAGE_PASSES)) {
+            item.quality = 0;
+        } else if (item.name.equals(SULFURAS)) {
+        } else {
+            decrementQuality(item);
+        }
+    }
+
+    private void decrementQuality(Item item) {
+        if (isItemQualityBelowZero(item)) {
+            item.quality -= 1;
+        }
+    }
+
+    private void incrementQuality(Item item) {
+        if (isItemQualityBelowMax(item)) {
+            item.quality += 1;
+        }
+    }
+
+    private boolean isItemQualityBelowZero(Item item) {
+        return item.quality > 0;
+    }
+
+    private boolean isItemQualityBelowMax(Item item) {
+        return item.quality < 50;
+    }
+
+    private boolean hasExpired(Item item) {
+        return item.sellIn < 0;
+    }
+
 }
